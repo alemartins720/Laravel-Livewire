4b6b3a668296d309cb8ea1bda6aec63271358f62
Alexandre Martins <alemartins720@gmail.com>
2025-05-19 14:47:43 -0300

    Aula 340-FullPage Components

---
+++ resources/views/livewire/full-page-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="display-6 text-center my-5">Componente Full Page</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 namespace App\Http\Controllers;
 
 use App\Livewire\Counter;
+use App\Livewire\FullPageComponent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/' , [MainController::class , 'home']);
-
-
 Route::get('/clients' , [MainController::class , 'showClients']);
+Route::get('/full-page' , FullPageComponent::class);
